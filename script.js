1b56782bf8e30448f2aab3ee506ea181f3037727
HussienHaji <hussienhaji.dev@gmail.com>
2024-02-16 12:14:26 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+
+$("#header-slider").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+$("#slider").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items:4,
+    },
+  },
+});
+
+$("#tutorial-slider").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items:4,
+    },
+  },
+});
